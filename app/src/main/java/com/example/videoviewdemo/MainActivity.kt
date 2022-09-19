ae2fbbc207f75f2e3ff65cdb674f74c134da5aff
waynetipsy <jacketoniru@gmail.com>
2022-09-19 22:07:52 +0100

    push

---
+++ app/src/main/java/com/example/videoviewdemo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.videoviewdemo
+
+import android.net.Uri
+import android.net.Uri.parse
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri : Uri = parse("android.resource://"+packageName+"/"+R.raw.wisdom)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+    }
+}
